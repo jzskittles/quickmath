6b7caa6137c43cd125f78fb3f8a0125b4f0fc1c1
Jenny Zhang <jz.skittles@gmail.com>
2018-06-25 11:08:54 -0500

    added 3 numbers format, changed main page, added pretty colors

--- app/src/main/java/com/mayseventhstudio/quickmath/AdditionSubtraction.java
+++ app/src/main/java/com/mayseventhstudio/quickmath/AdditionSubtraction.java
@@ -17,9 +17,11 @@ public class AdditionSubtraction extends AppCompatActivity {
 
     Button b1, b2, b3, b4, b5, b6, b7, b8,b9;
     String answer="";
+    int mode=0;
     TextView ans;
 
     TextView part1, part2, sign;
+    TextView q3part1, q3sign, q3part2, q3sign2, q3part3;
 
     int result = 0;
 
@@ -49,12 +51,43 @@ public class AdditionSubtraction extends AppCompatActivity {
         part2 = (TextView) findViewById(R.id.qpart2);
         sign = (TextView) findViewById(R.id.qsign);
 
+        q3part1 = (TextView) findViewById(R.id.q3part1);
+        q3sign = (TextView) findViewById(R.id.q3sign);
+        q3part2 = (TextView) findViewById(R.id.q3part2);
+        q3sign2 = (TextView) findViewById(R.id.q3sign2);
+        q3part3 = (TextView) findViewById(R.id.q3part3);
+
+
         streak = (TextView) findViewById(R.id.streak);
 
         bang = (ImageView) findViewById(R.id.bang);
 
-        generateNums();
+        mode = getIntent().getExtras().getInt("mode");
 
+        if(mode==2){
+            generateNums2();
+            q3part1.setVisibility(View.GONE);
+            q3sign.setVisibility(View.GONE);
+            q3part2.setVisibility(View.GONE);
+            q3sign2.setVisibility(View.GONE);
+            q3part3.setVisibility(View.GONE);
+
+            part1.setVisibility(View.VISIBLE);
+            sign.setVisibility(View.VISIBLE);
+            part2.setVisibility(View.VISIBLE);
+        }
+        else{
+            generateNums3();
+            q3part1.setVisibility(View.VISIBLE);
+            q3sign.setVisibility(View.VISIBLE);
+            q3part2.setVisibility(View.VISIBLE);
+            q3sign2.setVisibility(View.VISIBLE);
+            q3part3.setVisibility(View.VISIBLE);
+
+            part1.setVisibility(View.GONE);
+            sign.setVisibility(View.GONE);
+            part2.setVisibility(View.GONE);
+        }
     }
 
     public void getNum(View v) {
@@ -69,13 +102,13 @@ public class AdditionSubtraction extends AppCompatActivity {
         ans.setText(answer);
     }
 
-    public void generateNums(){
+    public void generateNums2(){
         Random rand = new Random();
         int a = rand.nextInt(101);
         int b = rand.nextInt(101);
 
-        int c = rand.nextInt(2);
-        if(c==0) {
+        int n = rand.nextInt(2);
+        if(n==0) {
             sign.setText("+");
             result = a+b;
         }
@@ -94,6 +127,66 @@ public class AdditionSubtraction extends AppCompatActivity {
         part2.setText(b+"");
     }
 
+    public void generateNums3(){
+        Random rand = new Random();
+        int a = rand.nextInt(101);
+        int b = rand.nextInt(101);
+        int c = rand.nextInt(101);
+
+        int n = rand.nextInt(2);
+        int o = rand.nextInt(2);
+        if(n==0) {
+            q3sign.setText("+");
+            result = a+b;
+        }
+        else {
+            q3sign.setText("-");
+            if(b>a){
+                int m = a;
+                a = b;
+                b = m;
+            }
+
+            result = a-b;
+        }
+
+        if(o==0){
+            q3sign2.setText("+");
+            result = result + c;
+            q3part1.setText(a+"");
+            q3part2.setText(b+"");
+            q3part3.setText(c+"");
+        }else{
+            q3sign2.setText("-");
+            if(c>result){
+                Toast.makeText(getApplicationContext(),"Negative!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),result+"",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),c+"",Toast.LENGTH_SHORT).show();
+                q3sign2.setText("+");
+                result = result + c;
+
+                /*int m = a;
+                a = c;
+                q3sign2.setText(q3sign.getText().toString());
+                q3sign.setText("-");
+                c=m;
+                if(q3sign2.getText().toString().equals("-"))
+                    result = a-b-c;
+                else
+                    result = a-b+c;*/
+                q3part1.setText(a+"");
+                q3part2.setText(b+"");
+                q3part3.setText(c+"");
+            }else{
+                result = result-c;
+                q3part1.setText(a+"");
+                q3part2.setText(b+"");
+                q3part3.setText(c+"");
+            }
+        }
+
+    }
+
     public void compare(View v){
         //Toast.makeText(getApplicationContext(),ans.getText().toString(), Toast.LENGTH_SHORT).show();
         //Toast.makeText(getApplicationContext(),result+"", Toast.LENGTH_SHORT).show();
@@ -106,10 +199,13 @@ public class AdditionSubtraction extends AppCompatActivity {
         streak.setText(streaknum+"");
         answer="";
         ans.setText(answer);
-        if(streaknum==5){
+        /*if(streaknum==5){
             bang.setVisibility(View.VISIBLE);
-        }
-        generateNums();
+        }*/
+        if(mode==2)
+            generateNums2();
+        else
+            generateNums3();
     }
 
 }
--- app/src/main/java/com/mayseventhstudio/quickmath/MainActivity.java
+++ app/src/main/java/com/mayseventhstudio/quickmath/MainActivity.java
@@ -8,20 +8,34 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button addsub;
+    Button addsub2, addsub3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addsub = (Button)findViewById(R.id.addsub);
-        addsub.setOnClickListener(new View.OnClickListener(){
+        addsub2 = (Button)findViewById(R.id.addsub2);
+        addsub2.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
                 Intent i = new Intent(getApplicationContext(), AdditionSubtraction.class);
+                Bundle b = new Bundle();
+                b.putInt("mode", 2);
+                i.putExtras(b);
+                startActivity(i);
+            }
+        });
 
+        addsub3 = (Button)findViewById(R.id.addsub3);
+        addsub3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AdditionSubtraction.class);
+                Bundle b = new Bundle();
+                b.putInt("mode", 3);
+                i.putExtras(b);
                 startActivity(i);
             }
         });
